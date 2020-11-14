f6108db4246c2812ec905a579eb0ec0ac76b513e
Túlio Albuquerque <tuliohsa87@gmail.com>
2020-11-14 15:33:18 -0300

    Obtendo um estoque por id

--- src/main/java/com/padaria/resources/EstoqueResource.java
+++ src/main/java/com/padaria/resources/EstoqueResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,12 @@ public class EstoqueResource {
 		List<Estoque> list = service.findAll();
 		List<EstoqueDTO> listDto = list.stream().map(x -> new EstoqueDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<EstoqueDTO> findById(@PathVariable String id){
+		Estoque obj =  service.findById(id);
+		return ResponseEntity.ok().body(new EstoqueDTO(obj));
 	}
 
 }
--- src/main/java/com/padaria/services/EstoqueService.java
+++ src/main/java/com/padaria/services/EstoqueService.java
@@ -1,12 +1,14 @@
 package com.padaria.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.padaria.domain.Estoque;
 import com.padaria.repository.EstoqueRepository;
+import com.padaria.services.exception.ObjectNotFoundException;
 
 @Service
 public class EstoqueService {
@@ -16,6 +18,11 @@ public class EstoqueService {
 	
 	public List<Estoque> findAll(){
 		return repo.findAll();
+	}
+	
+	public Estoque findById(String id) {
+		Optional<Estoque> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
